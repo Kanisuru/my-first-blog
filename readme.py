b3e98764934b6efe0203462e6a1ee5f512b8e9ab
kansure <kanisurudaniel@gmail.com>
2023-11-07 08:20:40 -0800

    My Django Work app, first commit

---
+++ readme.py
@@ -0,0 +1,12 @@
+#cd to a new directory
+#create a virtual environment = python -m venv {environment_name}
+#working with virtual env = {environment_name}\Scripts\activate
+#install django:
+#before installing lets get the latest version of the software 
+#used for installing it which "pip" = python -m pip install --upgrade pip
+#create a 'requirement.txt' file which keeps a list of dependencies to be installed
+#write 'Django~=3.2.10' in the 'requirement.txt' file
+#then run 'pip install -r requirements.txt' in the activated env
+#creating a project in django: 'django-admin.exe startproject mysite .'
+#the (dot) . is crucial because it tells the script to install
+# django to install the current directory 
